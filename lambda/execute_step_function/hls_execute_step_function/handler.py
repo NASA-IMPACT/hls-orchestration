c0162620d9ecff03784677f229b8083275405380
Sean Harkins <sharkinsgis@gmail.com>
2020-03-02 16:59:35 -0600

    Add unit tests for execute_step_function handler.

--- lambda/execute_step_function/hls_execute_step_function/handler.py
+++ lambda/execute_step_function/hls_execute_step_function/handler.py
@@ -5,24 +5,26 @@ from typing import Dict
 from botocore.errorfactory import ClientError
 from random import randint
 
-step_functions = boto3.client('stepfunctions')
-state_machine = os.getenv("STATE_MACHINE")
-
 
 def handler(event: Dict, context: Dict):
+    state_machine = os.getenv("STATE_MACHINE")
+    step_functions = boto3.client('stepfunctions')
     prefix = randint(100, 999)
-    # print(event['detail']['requestParameters']['key'])
-    # key = event['detail']['requestParameters']['key']
-    print(event['Records'][0]['s3']['object']['key'])
-    key = event['Records'][0]['s3']['object']['key']
-    keyroot = Path(key).stem
     try:
-        step_functions.start_execution(
-            stateMachineArn=state_machine,
-            name=f"{prefix}_{keyroot}",
-            input="{\"granule\": \"" + keyroot + "\"}"
-        )
-    except ClientError as e:
-        print(e)
+        # print(event['detail']['requestParameters']['key'])
+        # key = event['detail']['requestParameters']['key']
+        print(event['Records'][0]['s3']['object']['key'])
+        key = event['Records'][0]['s3']['object']['key']
+        keyroot = Path(key).stem
+        try:
+            reponse = step_functions.start_execution(
+                stateMachineArn=state_machine,
+                name=f"{prefix}_{keyroot}",
+                input="{\"granule\": \"" + keyroot + "\"}"
+            )
+            return reponse
+        except ClientError as ce:
+            print(ce)
 
-    return event
+    except KeyError:
+        print("Message body does not contain key")
---
+++ lambda/execute_step_function/tests/test_handler.py
@@ -0,0 +1,35 @@
+"""test handler."""
+
+import pytest
+from unittest.mock import patch
+from hls_execute_step_function.handler import handler
+
+
+def test_handler_keyError(capsys):
+    expected = "Message body does not contain key\n"
+    handler({}, {})
+    out, err = capsys.readouterr()
+    assert out == expected
+
+
+@patch("hls_execute_step_function.handler.boto3.client")
+def test_handler(client):
+    """Test handler."""
+    expected_input = '{"granule": "S2A_MSIL1C_20191001T201241_N0208_R028_T09WXT_20191001T220736A"}'
+    start_execution_response = {
+        "executionArn": "arn",
+        "startDate": 10
+    }
+    event = {
+        "Records": [{
+            "s3": {
+                "object": {
+                    "key": "S2A_MSIL1C_20191001T201241_N0208_R028_T09WXT_20191001T220736A.zip"
+                }
+            }
+        }]
+    }
+    client.return_value.start_execution.return_value = start_execution_response
+    handler(event, {})
+    args, kwargs = client.return_value.start_execution.call_args
+    assert kwargs["input"] == expected_input
