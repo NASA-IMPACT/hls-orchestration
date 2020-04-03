999869c02a0dd642e498b4f4a2585534be326973
Sean Harkins <sharkinsgis@gmail.com>
2020-04-03 13:38:20 -0600

    Remove granule id from step function execution name.

--- scripts/run_sentinel.sh
+++ scripts/run_sentinel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 granule=$1
 random=$RANDOM
-aws stepfunctions start-execution --state-machine-arn=$HLSSTACK_SENTINELSTATEMACHINEEXPORT --name ${random}_${1} --input "{\"granule\":\"${1}\"}"
+aws stepfunctions start-execution --state-machine-arn=$HLSSTACK_SENTINELSTATEMACHINEEXPORT --name ${random} --input "{\"granule\":\"${1}\"}"
