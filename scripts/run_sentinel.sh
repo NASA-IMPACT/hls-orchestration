e5825b31861feb880e74ff7986e57428f4f6a13d
Sean Harkins <sharkinsgis@gmail.com>
2020-05-01 11:04:19 -0500

    Setting source environment for setinel script.

--- scripts/run_sentinel.sh
+++ scripts/run_sentinel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+env_settings="$(dirname $DIR)/env.sh"
+source "$env_settings"
 granule=$1
 random=$RANDOM
 aws stepfunctions start-execution --state-machine-arn=$HLSSTACK_SENTINELSTATEMACHINEEXPORT --name ${random} --input "{\"granule\":\"${1}\"}"
