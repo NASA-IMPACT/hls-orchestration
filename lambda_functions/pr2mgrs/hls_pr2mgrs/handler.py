1e14e29b3af7205c7b8e7cceadae5f34ac767875
Sean Harkins <sharkinsgis@gmail.com>
2020-05-28 18:00:50 -0500

    Use lowercase keys to remove need for parameter transformation.

--- lambda_functions/pr2mgrs/hls_pr2mgrs/handler.py
+++ lambda_functions/pr2mgrs/hls_pr2mgrs/handler.py
@@ -18,8 +18,8 @@ with open(lookup_file, "r") as f:
 
 def handler(event: Dict, context: Dict):
     """AWS Lambda handler."""
-    if event.get("Row"):
-        pathrow = f"{event['Path']}{event['Row']}"
+    if event.get("row"):
+        pathrow = f"{event['path']}{event['row']}"
         listS2 = list(filter(lambda x: x[0] == pathrow, lookupTable))
         # Do we want to raise an error when no grid is found ?
         return [s2[1] for s2 in listS2]
@@ -27,11 +27,11 @@ def handler(event: Dict, context: Dict):
     elif event.get("MGRS"):
         listL8 = list(filter(lambda x: x[1] == str(event.get("MGRS")), lookupTable))
         pathrows = [l8[0] for l8 in listL8]
-        if event.get("Path"):
+        if event.get("path"):
             pathrows = [
                 pathrow
                 for pathrow in pathrows
-                if pathrow[0:3] == str(event.get("Path"))
+                if pathrow[0:3] == str(event.get("path"))
             ]
         # Do we want to raise an error when no grid is found ?
         return pathrows
--- lambda_functions/pr2mgrs/tests/test_handler.py
+++ lambda_functions/pr2mgrs/tests/test_handler.py
@@ -19,7 +19,7 @@ def test_handler():
         "31XEK",
         "31XEL",
     ]
-    assert handler({"Path": "001", "Row": "001"}, {}) == expected
+    assert handler({"path": "001", "row": "001"}, {}) == expected
 
     expected = [
         "001001",
@@ -54,6 +54,6 @@ def test_handler():
         "003001",
         "003002",
     ]
-    assert handler({"MGRS": "29XNK", "Path": "003"}, {}) == expected
+    assert handler({"MGRS": "29XNK", "path": "003"}, {}) == expected
     with pytest.raises(Exception):
         handler({}, {})
