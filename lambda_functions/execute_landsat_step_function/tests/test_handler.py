bf2473630f0721b278665d8bbb73fff72180ab0b
Sean Harkins <sharkinsgis@gmail.com>
2020-08-28 17:36:40 -0500

    Temporary test fix so that we can use T1 data for testing purposes.

--- lambda_functions/execute_landsat_step_function/tests/test_handler.py
+++ lambda_functions/execute_landsat_step_function/tests/test_handler.py
@@ -25,12 +25,12 @@ def test_handler(client):
         "processingMonth": "05",
         "processingDay": "27",
         "collectionNumber": "01",
-        "collectionCategory": "RT",
-        "scene": "LC08_L1GT_127010_20200527_20200527_01_RT",
+        "collectionCategory": "T1",
+        "scene": "LC08_L1GT_127010_20200527_20200527_01_T1",
         "date": "2020-05-27",
         "scheme": "s3",
         "bucket": "landsat-pds",
-        "prefix": "c1/L8/127/010/LC08_L1GT_127010_20200527_20200527_01_RT",
+        "prefix": "c1/L8/127/010/LC08_L1GT_127010_20200527_20200527_01_T1",
     }
     expected_input = json.dumps(scene_meta)
     start_execution_response = {"executionArn": "arn", "startDate": 10}
@@ -38,7 +38,8 @@ def test_handler(client):
         "Records": [
             {
                 "Sns": {
-                    "Message": '{"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"us-west-2","eventTime":"2016-01-16T01:36:55.014Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAILHHXPNIKSGVUGOZK"},"requestParameters":{"sourceIPAddress":"52.27.39.85"},"responseElements":{"x-amz-request-id":"078952E6C7CC52B4","x-amz-id-2":"Xboo1ULzd7PxY27iIaGXjUStV8TmG52JAbiWQpiRJWuRqfaBhLcc0XMUKNmXgd5fbIfRd1IcrgE="},"s3":{"s3SchemaVersion":"1.0","configurationId":"NewHTML","bucket":{"name":"landsat-pds","ownerIdentity":{"principalId":"A3LZTVCZQ87CNW"},"arn":"arn:aws:s3:::landsat-pds"},"object":{"key":"c1/L8/127/010/LC08_L1GT_127010_20200527_20200527_01_RT/index.html","size":3780,"eTag":"736e4e5a36cb8a1c6cbfc58659126ff1","sequencer":"0056999EB6F8BDBB8D"}}}]}',
+                    "Message":
+                    '{"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"us-west-2","eventTime":"2016-01-16T01:36:55.014Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAILHHXPNIKSGVUGOZK"},"requestParameters":{"sourceIPAddress":"52.27.39.85"},"responseElements":{"x-amz-request-id":"078952E6C7CC52B4","x-amz-id-2":"Xboo1ULzd7PxY27iIaGXjUStV8TmG52JAbiWQpiRJWuRqfaBhLcc0XMUKNmXgd5fbIfRd1IcrgE="},"s3":{"s3SchemaVersion":"1.0","configurationId":"NewHTML","bucket":{"name":"landsat-pds","ownerIdentity":{"principalId":"A3LZTVCZQ87CNW"},"arn":"arn:aws:s3:::landsat-pds"},"object":{"key":"c1/L8/127/010/LC08_L1GT_127010_20200527_20200527_01_T1/index.html","size":3780,"eTag":"736e4e5a36cb8a1c6cbfc58659126ff1","sequencer":"0056999EB6F8BDBB8D"}}}]}',
                 }
             }
         ]
@@ -50,21 +51,21 @@ def test_handler(client):
     assert kwargs["input"] == expected_input
 
 
-@patch(
-    "lambda_functions.execute_landsat_step_function.hls_execute_landsat_step_function.handler.boto3.client"
-)
-def test_handler_non_RT(client):
-    """Test handler."""
+# @patch(
+    # "lambda_functions.execute_landsat_step_function.hls_execute_landsat_step_function.handler.boto3.client"
+# )
+# def test_handler_non_RT(client):
+    # """Test handler."""
 
-    event = {
-        "Records": [
-            {
-                "Sns": {
-                    "Message": '{"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"us-west-2","eventTime":"2016-01-16T01:36:55.014Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAILHHXPNIKSGVUGOZK"},"requestParameters":{"sourceIPAddress":"52.27.39.85"},"responseElements":{"x-amz-request-id":"078952E6C7CC52B4","x-amz-id-2":"Xboo1ULzd7PxY27iIaGXjUStV8TmG52JAbiWQpiRJWuRqfaBhLcc0XMUKNmXgd5fbIfRd1IcrgE="},"s3":{"s3SchemaVersion":"1.0","configurationId":"NewHTML","bucket":{"name":"landsat-pds","ownerIdentity":{"principalId":"A3LZTVCZQ87CNW"},"arn":"arn:aws:s3:::landsat-pds"},"object":{"key":"/c1/L8/139/045/LC08_L1TP_139045_20170304_20170316_01_T1/index.html","size":3780,"eTag":"736e4e5a36cb8a1c6cbfc58659126ff1","sequencer":"0056999EB6F8BDBB8D"}}}]}',
-                }
-            }
-        ]
-    }
+    # event = {
+        # "Records": [
+            # {
+                # "Sns": {
+                    # "Message": '{"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"us-west-2","eventTime":"2016-01-16T01:36:55.014Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAILHHXPNIKSGVUGOZK"},"requestParameters":{"sourceIPAddress":"52.27.39.85"},"responseElements":{"x-amz-request-id":"078952E6C7CC52B4","x-amz-id-2":"Xboo1ULzd7PxY27iIaGXjUStV8TmG52JAbiWQpiRJWuRqfaBhLcc0XMUKNmXgd5fbIfRd1IcrgE="},"s3":{"s3SchemaVersion":"1.0","configurationId":"NewHTML","bucket":{"name":"landsat-pds","ownerIdentity":{"principalId":"A3LZTVCZQ87CNW"},"arn":"arn:aws:s3:::landsat-pds"},"object":{"key":"/c1/L8/139/045/LC08_L1TP_139045_20170304_20170316_01_T1/index.html","size":3780,"eTag":"736e4e5a36cb8a1c6cbfc58659126ff1","sequencer":"0056999EB6F8BDBB8D"}}}]}',
+                # }
+            # }
+        # ]
+    # }
 
-    handler(event, {})
-    client.return_value.start_execution.assert_not_called()
+    # handler(event, {})
+    # client.return_value.start_execution.assert_not_called()
