aacfc424b0fe6203b10a44e1c11a2881d97e5f9a
Sean Harkins <sharkinsgis@gmail.com>
2020-08-29 13:40:55 -0500

    Update scripts to use environment.sh.

--- scripts/run_sentinel_task.sh
+++ scripts/run_sentinel_task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-env_settings="$(dirname $DIR)/env.sh"
+env_settings="$(dirname $DIR)/environment.sh"
 source "$env_settings"
 granule=$1
 random=$RANDOM
--- scripts/run_sentinel_test_granules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for file in `cat sentinel_test_granules.txt`; do echo ${file} && ./run_sentinel.sh $file; done
-
