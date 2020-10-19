efdfae65e4388d75f213d2a214d2a44a35b28e17
Sean Harkins <sharkinsgis@gmail.com>
2020-10-19 11:15:52 -0500

    Specify explicit input bucket to support validation granules bucket.

--- scripts/run_validation_granules.sh
+++ scripts/run_validation_granules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-listresult=$(aws s3 ls hls-sentinel-validation-scenes)
+listresult=$(aws s3 ls hls-sentinel-validation-scenes/cloud_free_google2/)
 scenes=$(echo "$listresult" | awk '{print $4}')
 for scene in $scenes; do
   if [ "${scene:0:1}" != "_" ]; then
     granule=$(basename "$scene" .zip)
-    sh "${DIR}/run_sentinel_granule_debug.sh" "$granule"
+    sh "${DIR}/run_sentinel_granule_debug.sh" "$granule" hls-sentinel-validation-scenes/cloud_free_google2
   fi
 done
