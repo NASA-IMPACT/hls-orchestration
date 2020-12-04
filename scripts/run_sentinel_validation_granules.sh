b2da484ac08bfdf4ae534f09aa03fbc86bf45edb
Sean Harkins <sharkinsgis@gmail.com>
2020-12-04 12:34:48 -0600

    Clean up validation data scripts.

--- scripts/run_sentinel_validation_granules.sh
+++ scripts/run_sentinel_validation_granules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-listresult=$(aws s3 ls hls-sentinel-validation-scenes/cloud_free_google2/)
+listresult=$(aws s3 ls hls-sentinel-validation-scenes/cloud_free_google/)
 scenes=$(echo "$listresult" | awk '{print $4}')
 for scene in $scenes; do
   if [ "${scene:0:1}" != "_" ]; then
     granule=$(basename "$scene" .zip)
-    sh "${DIR}/run_sentinel_granule_debug.sh" "$granule" hls-sentinel-validation-scenes/cloud_free_google2
+    sh "${DIR}/run_sentinel_granule_debug.sh" "$granule" hls-sentinel-validation-scenes/cloud_free_google
   fi
 done
