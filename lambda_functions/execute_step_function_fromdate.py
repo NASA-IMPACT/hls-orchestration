d24bb70bd9914e088b42e8a023ea4233657dd485
Sean Harkins <sharkinsgis@gmail.com>
2021-01-13 16:22:37 -0600

    Add step_function_trigger with fromdate input.

---
+++ lambda_functions/execute_step_function_fromdate.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+from pathlib import Path
+from typing import Dict
+from botocore.errorfactory import ClientError
+from datetime import datetime, timedelta
+
+
+def handler(event: Dict, context: Dict):
+    date_delta = int(os.getenv("DAYS_PRIOR"))
+    state_machine = os.getenv("STATE_MACHINE")
+    step_functions = boto3.client("stepfunctions")
+    event_time = datetime.strptime(event["time"], '%Y-%m-%dT%H:%M:%SZ')
+    fromdate = (event_time - timedelta(days=date_delta)).strftime('%d/%m/%Y')
+    step_input = '{{"fromdate": "{0}"}}'.format(fromdate)
+    print(step_input)
+    try:
+        step_functions.start_execution(
+            stateMachineArn=state_machine,
+            input=step_input,
+        )
+        return event
+    except ClientError as ce:
+        print(ce)
