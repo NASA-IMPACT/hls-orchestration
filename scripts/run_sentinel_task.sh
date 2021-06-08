f143253704ffadf2971c747736518fea3ce00f16
sharkinsspatial <sharkinsgis@gmail.com>
2021-06-08 18:45:50 -0500

    Remove legacy sentinel task shell script.

--- scripts/run_sentinel_task.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-env_settings="$(dirname $DIR)/environment.sh"
-source "$env_settings"
-granule=$1
-random=$RANDOM
-aws stepfunctions start-execution --state-machine-arn=$HLSSTACK_SENTINELSTATEMACHINEEXPORT --name ${random} --input "{\"granule\":\"${1}\"}"
