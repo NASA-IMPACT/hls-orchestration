217fc52d53daf890fed6efd71a621c9f5dbc0058
sharkinsspatial <sharkinsgis@gmail.com>
2021-09-17 14:32:15 -0500

    Fix linting issue.

--- stack/stack.py
+++ stack/stack.py
@@ -122,7 +122,6 @@ else:
     GCC = False
 
 
-
 class HlsStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
