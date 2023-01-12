b2358059329579b9082ec5cec0c8053d6c34d53c
sharkinsspatial <sharkinsgis@gmail.com>
2023-01-12 12:37:14 -0600

    Create job definitions for specific PRs for integration testing.

--- scripts/run_sentinel_validation.py
+++ scripts/run_sentinel_validation.py
@@ -6,13 +6,17 @@ import boto3
 
 client = boto3.client("batch")
 jobqueue = os.getenv("HLSSTACK_SENTINELJOBQUEUEEXPORT")
-jobdefinition = os.getenv("HLSSTACK_SENTINELJOBDEFINITION")
 #  Short running granule
 #  S2B_MSIL1C_20200806T173909_N0209_R098_T13TFN_20200806T195018
 
 run_id = sys.argv[1]
 input_bucket = sys.argv[2]
 granules = sys.argv[3]
+
+if len(sys.argv) == 5:
+    jobdefinition = sys.argv[4]
+else:
+    jobdefinition = os.getenv("HLSSTACK_SENTINELJOBDEFINITION")
 
 
 def submit_job(granule_id):
