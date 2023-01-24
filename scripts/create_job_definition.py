4b3fbdd5800a8eae27bacd4716c2087f7251f437
sharkinsspatial <sharkinsgis@gmail.com>
2023-01-23 18:03:48 -0600

    Modify run_landsat_validation to support PR job definitions.

--- scripts/create_job_definition.py
+++ scripts/create_job_definition.py
@@ -9,6 +9,8 @@ if image == "hls-laads":
     role_arn = "arn:aws:iam::018923174646:role/hls-development-LaadsTaskTaskRoleC8A42E20-2V99O0N3RMJ0"
 if image == "hls-sentinel":
     role_arn = "arn:aws:iam::018923174646:role/hls-development-SentinelTaskTaskRoleEB97BB3F-1AIL1PXLA4JKJ"
+if image == "hls-landsat-c2":
+    role_arn = "arn:aws:iam::018923174646:role/hls-development-LandsatTaskTaskRoleFD2391A2-440VUZKTYZ2O"
 
 
 client = boto3.client("batch")
