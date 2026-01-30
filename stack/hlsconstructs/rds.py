9dd12c77ee65c533de84497b4f5dec7f42ad8d91
Chris Holden <ceholden@gmail.com>
2026-01-30 15:08:24 -0500

    fix: relax to major version to avoid drift

--- stack/hlsconstructs/rds.py
+++ stack/hlsconstructs/rds.py
@@ -56,11 +56,12 @@ class Rds(Construct):
         )
 
         self.database_name = "hls"
+
         self.database = aws_rds.DatabaseCluster(
             self,
             "RdsCluster",
             engine=aws_rds.DatabaseClusterEngine.aurora_postgres(
-                version=aws_rds.AuroraPostgresEngineVersion.VER_13_12,
+                version=aws_rds.AuroraPostgresEngineVersion.of("13", "13"),
             ),
             default_database_name=self.database_name,
             enable_data_api=True,
